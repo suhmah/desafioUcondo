e5f01d4a6bdb2193bbc305ed98e005899f154596
Matheus <mhlreis@rd.com.br>
2025-03-20 11:10:54 -0300

    feat: plano de contas

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/navigation';
+import { HeaderProvider } from './src/hooks/HeaderInputContext';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <HeaderProvider>
         <Routes />
+      </HeaderProvider>
     </NavigationContainer>
   );
 }
